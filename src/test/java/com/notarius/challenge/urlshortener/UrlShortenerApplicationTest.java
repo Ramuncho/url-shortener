99c4c5cd0866b7469de3aa4328fd2c72fe43f5c4
RDejean <dejean.rom@gmail.com>
2023-10-16 23:07:51 -0400

    fix docker compose

--- src/test/java/com/notarius/challenge/urlshortener/UrlShortenerApplicationTest.java
+++ src/test/java/com/notarius/challenge/urlshortener/UrlShortenerApplicationTest.java
@@ -1,14 +1,10 @@
 package com.notarius.challenge.urlshortener;
 
-import com.notarius.challenge.urlshortener.service.ShorteningService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UrlShortenerApplicationTest {
-    @Autowired
-    private ShorteningService shorteningService;
 
     @Test
     void contextLoads() {
